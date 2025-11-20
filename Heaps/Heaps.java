5f1fe4da19449da1b3c0f0985f4f7ab5bd913a05
yamanvermav20 <yamanverma083@gmail.com>
2025-11-20 12:25:52 +0530

    Heaps

--- Heaps/Heaps.java
+++ Heaps/Heaps.java
@@ -37,6 +37,52 @@ public class Heaps {
                 par = (x - 1) / 2;
             }
         }
+
+        public int peek(){
+            return arr.get(0);
+        }
+
+        private void heapify (int i){
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            int minIdx = i;
+
+            if(left < arr.size() && arr.get(minIdx) > arr.get(left)){
+                minIdx = left;
+            }
+            
+            if(right < arr.size() && arr.get(minIdx) > arr.get(right)){
+                minIdx = right;
+            }
+
+            if(minIdx != i){
+                //swap
+                int temp = arr.get(i);
+                arr.set(i, arr.get(minIdx));
+                arr.set(minIdx, temp);
+
+                heapify(minIdx);
+            }
+
+        }
+        public int remove(){
+            int data = arr.get(0);
+
+            //step1 = swap first and last
+            int temp = arr.get(0);
+            arr.set(0, arr.get(arr.size() - 1));
+            arr.set(arr.size() - 1, temp);
+
+            //step2 = delte last
+            arr.remove(arr.size() - 1);
+
+            //step3 - heapify
+            heapify(0);
+            return data;
+        } 
+        //For 1st and last node swap
+        // remove last index arr.remove(arr.size() - 1);
+        // fix my heap function heapify 
     }
     public static void main(String[] args){
         
