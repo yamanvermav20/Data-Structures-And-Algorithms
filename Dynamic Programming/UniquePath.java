23ac2c35f71f4e0d7cfac9bba8e7d714f27d8015
yamanvermav20 <yamanverma083@gmail.com>
2025-11-29 22:52:39 +0530

    uniquepath memoization + tabulation

--- Dynamic Programming/UniquePath.java
+++ Dynamic Programming/UniquePath.java
@@ -1,21 +1,23 @@
 public class UniquePath {
     //tabulation
-    // public static int uniquePaths(int m, int n) {
-    //     int[][] dp = new int[m][n];
-    //     for(int i = 0; i < m; i++){
-    //         dp[i][0] = 1;
-    //     }
-    //     for(int j = 0; j < n; j++){
-    //         dp[0][j] = 1;
-    //     }
-    //     for(int i = 1; i < m; i++){
-    //         for(int j = 1; j < n; j++){
-    //             dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-    //         }
-    //     }
-    //     return dp[m - 1][n - 1];
-    // }  
-    public int path(int i, int j, int[][] dp){
+    public static int uniquePathsMemo(int m, int n) {
+        int[][] dp = new int[m][n];
+        for(int i = 0; i < m; i++){
+            dp[i][0] = 1;
+        }
+        for(int j = 0; j < n; j++){
+            dp[0][j] = 1;
+        }
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }  
+
+    //memoization
+    public static int path(int i, int j, int[][] dp){
         if(i == 0 || j == 0) return 1;
 
         if(i < 0 || j < 0) return 0;
@@ -27,11 +29,20 @@ public class UniquePath {
         return dp[i][j];
 
     }
-    public int uniquePaths(int m, int n) {
+    public static int uniquePathsTab(int m, int n) {
         int[][] dp = new int[m][n];
         return path(m - 1, n - 1, dp);
     }  
     public static void main(String args[]){
-        
+        int m = 3;
+        int n = 7;
+
+        // Test Tabulation
+        int tabResult = uniquePathsTab(m, n);
+        System.out.println("Tabulation Result: " + tabResult);
+
+        // Test Memoization
+        int memoResult = uniquePathsMemo(m, n);
+        System.out.println("Memoization Result: " + memoResult);
     }
 }
