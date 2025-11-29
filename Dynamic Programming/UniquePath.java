2993da456fc343932f9e19b2b0fd329f456223ac
yamanvermav20 <yamanverma083@gmail.com>
2025-11-29 22:50:27 +0530

    uniquepath tabulation

--- Dynamic Programming/UniquePath.java
+++ Dynamic Programming/UniquePath.java
@@ -14,7 +14,23 @@ public class UniquePath {
             }
         }
         return dp[m - 1][n - 1];
-    }    
+    }  
+    public int path(int i, int j, int[][] dp){
+        if(i == 0 || j == 0) return 1;
+
+        if(i < 0 || j < 0) return 0;
+        if(dp[i][j] != 0) return dp[i][j];
+        int left = path(i - 1, j, dp);
+        int right = path(i, j - 1, dp);
+        
+        dp[i][j] = left + right;
+        return dp[i][j];
+
+    }
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        return path(m - 1, n - 1, dp);
+    }  
     public static void main(String args[]){
         
     }
