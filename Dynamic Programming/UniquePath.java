f3c65a9ddd6bc43e92bdb7d56569ba7fa10e8ff0
yamanvermav20 <yamanverma083@gmail.com>
2025-11-29 22:53:14 +0530

    uniquepath memoization + tabulation

--- Dynamic Programming/UniquePath.java
+++ Dynamic Programming/UniquePath.java
@@ -1,5 +1,5 @@
 public class UniquePath {
-    //tabulation
+    //tabulation Code
     public static int uniquePathsMemo(int m, int n) {
         int[][] dp = new int[m][n];
         for(int i = 0; i < m; i++){
@@ -16,7 +16,7 @@ public class UniquePath {
         return dp[m - 1][n - 1];
     }  
 
-    //memoization
+    //memoization Code
     public static int path(int i, int j, int[][] dp){
         if(i == 0 || j == 0) return 1;
 
