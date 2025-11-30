e88ed6951e26f76d5a02797edce25d4fce78aeb0
yamanvermav20 <yamanverma083@gmail.com>
2025-11-30 11:26:58 +0530

    Has Path Graph

--- Graphs/HasPath.java
+++ Graphs/HasPath.java
@@ -1,5 +1,70 @@
+import java.util.ArrayList;
+
 public class HasPath {
+    static class Edge{
+        int src;
+        int dest;
+        public Edge(int s, int d){
+            this.src = s;
+            this.dest = d;
+        }
+    }
+    static void createGraph(ArrayList<Edge> graph[]){
+        for(int i = 0; i < graph.length; i++){
+            graph[i] = new ArrayList<>();
+        }
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
+
+        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 3));
+
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 4));
+
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 4));
+        graph[3].add(new Edge(3, 5));
+
+        graph[4].add(new Edge(4, 2));
+        graph[4].add(new Edge(4, 3));
+        graph[4].add(new Edge(4, 5));
+
+        graph[5].add(new Edge(5, 3));
+        graph[5].add(new Edge(5, 4));
+        graph[5].add(new Edge(5, 6));
+
+        graph[6].add(new Edge(6, 5));
+    }
+    public static boolean dfs(ArrayList<Edge>[] graph, int curr, boolean[] visited, int src, int dest){
+        if(curr == dest || src == dest) return true;
+
+        visited[curr] = true;
+        for(int i = 0; i < graph[curr].size(); i++){
+            Edge e = graph[curr].get(i);
+            if(dest == e.dest) return true;
+            if(!visited[e.dest]){
+                if(dfs(graph, e.dest, visited, src, dest)) return true;;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args){
+        /*
+            1 ---- 3
+            /      | \
+           0       |  5 --- 6
+            \      |  /
+             2 ----4 
+
+         */
+        int V = 7;
+        ArrayList<Edge>[] graph = new ArrayList[V];
+        createGraph(graph);
+
+        boolean[] visited = new boolean[V];
         
+        boolean hasPath = dfs(graph, 0, visited, 0, 6);
+        System.out.println(hasPath);
     }
 }
