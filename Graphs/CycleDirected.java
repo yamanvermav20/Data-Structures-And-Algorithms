5d143f356b937261334be2bb6647a2f2ad1f022a
yamanvermav20 <yamanverma083@gmail.com>
2025-12-01 23:24:07 +0530

    Cycle for Directed

--- Graphs/CycleDirected.java
+++ Graphs/CycleDirected.java
@@ -1,6 +1,15 @@
 import java.util.ArrayList;
 
 public class CycleDirected {
+    static class Edge{
+        int src;
+        int dest;
+        
+        public Edge(int s, int d){
+            this.src = s;
+            this.dest = d;
+        }
+    }
     static void createGraph(ArrayList<Edge> graph[]){
         for(int i = 0; i < graph.length; i++){
             graph[i] = new ArrayList<>();
