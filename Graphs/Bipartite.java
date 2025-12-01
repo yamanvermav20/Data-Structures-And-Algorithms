4730ab5d88b86031de897eaaf711783c6a7cbca1
yamanvermav20 <yamanverma083@gmail.com>
2025-12-01 22:54:55 +0530

    Bipartite graph create to check

--- Graphs/Bipartite.java
+++ Graphs/Bipartite.java
@@ -2,6 +2,46 @@
 //nodes forming edge are in different sets
 //if this possible for all nodes
 //then its a bipartitie graph
+
+import java.util.ArrayList;
+
 public class Bipartite {
-    
+    static class Edge{
+        int src;
+        int dest;
+        public Edge(int s, int d){
+            this.src = s;
+            this.dest = d;
+        }
+    }
+    static void createGraph(ArrayList<Edge> graph[]){
+        for(int i = 0; i < graph.length; i++){
+            graph[i] = new ArrayList<>();
+        }
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
+
+        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 3));
+        graph[1].add(new Edge(1, 4));  
+
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 4));
+
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 4));
+
+        graph[4].add(new Edge(4, 2));
+        graph[4].add(new Edge(4, 3));
+        graph[4].add(new Edge(4, 1));   
+        graph[5].add(new Edge(5, 6));
+
+        graph[6].add(new Edge(6, 5));
+    }
+    public static void main(String[] args){
+        int V = 7;
+        ArrayList<Edge>[] graph = new ArrayList[V];
+        createGraph(graph);
+
+    }
 }
