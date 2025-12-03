de40fe85a85c069b9436f2838a5adbdac1c451a5
yamanvermav20 <yamanverma083@gmail.com>
2025-12-03 22:24:52 +0530

    Lecture

--- Graphs/CycleDirected.java
+++ Graphs/CycleDirected.java
@@ -21,21 +21,50 @@ public class CycleDirected {
         graph[1].add(new Edge(1, 3));
 
         graph[2].add(new Edge(2, 0));
-        // graph[2].add(new Edge(2, 4));
+        graph[2].add(new Edge(2, 4));
 
         graph[3].add(new Edge(3, 1));
-        // graph[3].add(new Edge(3, 4));
-        // graph[3].add(new Edge(3, 5));
+        graph[3].add(new Edge(3, 4));
+        graph[3].add(new Edge(3, 5));
 
-        // graph[4].add(new Edge(4, 2));
-        // graph[4].add(new Edge(4, 3));
-        // graph[4].add(new Edge(4, 5));
+        graph[4].add(new Edge(4, 2));
+        graph[4].add(new Edge(4, 3));
+        graph[4].add(new Edge(4, 5));
 
-        // graph[5].add(new Edge(5, 3));
-        // graph[5].add(new Edge(5, 4));
-        // graph[5].add(new Edge(5, 6));
+        graph[5].add(new Edge(5, 3));
+        graph[5].add(new Edge(5, 4));
+        graph[5].add(new Edge(5, 6));
 
-        // graph[6].add(new Edge(6, 5));
+        graph[6].add(new Edge(6, 5));
+    }
+    public static boolean isCycle(ArrayList<Edge>[] graph){
+        boolean visited[] = new boolean[graph.length];
+        boolean stack[] = new boolean[graph.length];
+
+        for(int i = 0; i < graph.length; i++){
+            if(!visited[i]){
+                if(isCycleUtil(graph, i, visited, stack)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static boolean isCycleUtil(ArrayList<Edge>[] graph, int curr, boolean[] visited, boolean[] stack){
+        visited[curr] = true;
+        stack[curr] = true;
+
+        for(int i = 0; i < graph[curr].size(); i++){
+            Edge e = graph[curr].get(i);
+            if(stack[e.dest]){
+                return true;
+            }
+            if(!visited[e.dest] && isCycleUtil(graph, e.dest, visited, stack)){
+                return true;
+            }
+        }
+        stack[curr] = false;
+        return false;
     }
     public static void main(String[] args){
         /*
@@ -49,5 +78,7 @@ public class CycleDirected {
         int V = 7;
         ArrayList<Edge> graph[] = new ArrayList[V];
         createGraph(graph);
+
+        System.out.println(isCycle(graph));
     }
 }
