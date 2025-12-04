1c9c41845d2947dd021db84ebab661afce78ce45
yamanvermav20 <yamanverma083@gmail.com>
2025-12-04 15:07:38 +0530

    Topological Sort

--- Graphs/CycleDirected.java
+++ Graphs/CycleDirected.java
@@ -15,28 +15,27 @@ public class CycleDirected {
             graph[i] = new ArrayList<>();
         }
         graph[0].add(new Edge(0, 1));
-        graph[0].add(new Edge(0, 2));
+        // graph[0].add(new Edge(0, 2));
 
-        graph[1].add(new Edge(1, 0));
-        graph[1].add(new Edge(1, 3));
+        // graph[1].add(new Edge(1, 0));
+        // graph[1].add(new Edge(1, 3));
 
-        graph[2].add(new Edge(2, 0));
-        graph[2].add(new Edge(2, 4));
+        // graph[2].add(new Edge(2, 0));
+        // graph[2].add(new Edge(2, 4));
 
-        graph[3].add(new Edge(3, 1));
-        graph[3].add(new Edge(3, 4));
-        graph[3].add(new Edge(3, 5));
+        // graph[3].add(new Edge(3, 1));
+        // graph[3].add(new Edge(3, 4));
+        // graph[3].add(new Edge(3, 5));
 
-        graph[4].add(new Edge(4, 2));
-        graph[4].add(new Edge(4, 3));
-        graph[4].add(new Edge(4, 5));
+        // graph[4].add(new Edge(4, 2));
+        // graph[4].add(new Edge(4, 3));
+        // graph[4].add(new Edge(4, 5));
 
-        graph[5].add(new Edge(5, 3));
-        graph[5].add(new Edge(5, 4));
-        graph[5].add(new Edge(5, 6));
+        // graph[5].add(new Edge(5, 3));
+        // graph[5].add(new Edge(5, 4));
+        // graph[5].add(new Edge(5, 6));
 
-        graph[6].add(new Edge(6, 5));
-        graph[6].add(new Edge(6, 5));
+        // graph[6].add(new Edge(6, 5));
     }
     public static boolean isCycle(ArrayList<Edge>[] graph){
         boolean visited[] = new boolean[graph.length];
