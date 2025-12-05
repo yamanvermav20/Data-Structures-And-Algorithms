944750f38672f096f41131bc71dd24099a99e3a4
yamanvermav20 <yamanverma083@gmail.com>
2025-12-05 22:11:43 +0530

    kth largest

---
+++ Heaps/kthLargest.java
@@ -0,0 +1,17 @@
+import java.util.PriorityQueue;
+
+public class kthLargest {
+    public static kthLargest
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        pq.add(3);
+        pq.add(32);
+        pq.add(32);
+        pq.add(63);
+        pq.add(73);
+        pq.add(13);
+        pq.add(3);
+
+    }
+}
