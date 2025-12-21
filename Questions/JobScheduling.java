382d5da3ad201f3850472746f131b2f28bfd02e8
yamanvermav20 <yamanverma083@gmail.com>
2025-12-21 13:58:51 +0530

    Job Scheduling Corrected

--- Questions/JobScheduling.java
+++ Questions/JobScheduling.java
@@ -8,6 +8,7 @@ public class JobScheduling {
 
         if(n == 0){
             System.out.println(0);
+            sc.close();
             return;
         }
 
@@ -17,22 +18,20 @@ public class JobScheduling {
         }
 
         Arrays.sort(arr, (a, b) -> {
-            if(a[0] != b[0]){
-                return a[0] - b[0];
-            }
-            else{
                 return b[1] - a[1];
-            }
         });
 
-        int maxDeadline = arr[n - 1][0];
-
+        int maxDeadline = 0;
+        for (int i = 0; i < n; i++) {
+            maxDeadline = Math.max(maxDeadline, arr[i][0]);
+        }
+        
         int[] slot = new int[maxDeadline + 1];
         Arrays.fill(slot, -1);
 
         int maxProfit = 0;
 
-        for(int i = n - 1; i >= 0; i--){
+        for(int i = 0; i < n; i++){
             int deadline = arr[i][0];
             int profit = arr[i][1];
 
@@ -50,5 +49,6 @@ public class JobScheduling {
         }
 
         System.out.println(maxProfit);
+        sc.close();
     }
 }
