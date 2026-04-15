95576e47d2fd6426408c0d941d22662939866a58
yamanvermav20 <yamanverma083@gmail.com>
2026-04-16 00:33:48 +0530

    Morgan Stanley

--- Questions/Morgan Stanley/Main.java
+++ Questions/Morgan Stanley/Main.java
@@ -7,41 +7,33 @@ public class Main {
 
     //arr = [8, 4, 2, 2, 2, 3];
     // we have to maximize the trp
+    //Questions is  same as Leetcode Delete and Earn;
 
-    public static int solve(int[] arr, int index, HashSet<Integer> set, int value){
-        if(index == arr.length){
-            return 0;
-        }
-        if(set.isEmpty()){
-            return 0;
-        }
-
-        set.remove(arr[index] + 1);
-        set.remove(arr[index] - 1);
-        int pick = 0;
-        if(set.contains(arr[index])) pick = arr[index] + solve(arr, index + 1, set, value + arr[index]);
-        set.add(arr[index] + 1);
-        set.add(arr[index] - 1);
-
-        int notpick = solve(arr, index + 1, set, value);
-
-        return Math.max(pick, notpick);
-
-    }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n  = sc.nextInt();
         int[] arr = new int[n];
-        HashSet<Integer> set = new HashSet<>();
+        int max = 0;
 
         for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
-            set.add(arr[i]);
+            max = Math.max(max, arr[i]);
+        }
+        
+        int[] arr2 = new int[max + 1];
+        for(int i : arr){
+            arr2[i] += i;
+        }
+        
+        int[] dp = new int[max + 1];
+        dp[0] = arr2[0];
+        dp[1] = Math.max(arr2[0], arr2[1]);
+
+        for(int i = 2; i < max + 1 ; i++){
+            dp[i] = Math.max(arr2[i] + dp[i - 2], dp[i - 1]);
         }
 
-        
-        int ans = solve(arr, 0, set, 0);
-        System.out.print(ans);
+        System.out.print(dp[max]);
     }
 }
 
